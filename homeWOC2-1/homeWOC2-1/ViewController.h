76f879c1f31ca0b0f7a594d62af44571c53a358b
AnnaKorr <7319162@gmail.com>
2017-03-01 14:49:07 +0300

    Add files via upload

---
+++ homeWOC2-1/homeWOC2-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homeWOC2-1
+//
+//  Created by apple on 01.03.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ homeWOC2-1/homeWOC2-1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  homeWOC2-1
+//
+//  Created by apple on 01.03.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
