6b2722d2e5eeaa33bd4db883423a155c2f7be1a7
AnnaKorr <7319162@gmail.com>
2017-03-02 13:36:39 +0300

    2 stringz

--- homeWOC2-1/homeWOC2-1/main.m
+++ homeWOC2-1/homeWOC2-1/main.m
@@ -13,6 +13,9 @@ int main(int argc, char * argv[]) {
     @autoreleasepool {
         
         NSLog(@"Не забываем пушить и коммитить");
+        NSLog(@"Не забываем пушить и коммитить");
+        NSLog(@"Не забываем пушить и коммитить");
+
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
