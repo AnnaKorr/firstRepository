c2c7fcc7942a4cfb276395b2d90ff8bde678a041
AnnaKorr <7319162@gmail.com>
2017-03-02 13:08:02 +0300

    homeWOC2-1

--- homeWOC2-1/homeWOC2-1/main.m
+++ homeWOC2-1/homeWOC2-1/main.m
@@ -11,6 +11,8 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        NSLog(@"Не забываем пушить и коммитить");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
